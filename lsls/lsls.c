bdcb4544d28a7515aedcf10e9f5017bbfafcf8d1
jeffyjkang <jeffyjkang@gmail.com>
2019-03-22 09:49:37 -0700

    2nd part

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -19,14 +19,15 @@ int main(int argc, char **argv)
   struct dirent *de;
   // init pointer for dir name
   char *dirname;
-
+  // init struct type stat, call to statbuf
+  struct stat statbuf;
   // if num of args is greater than two, err out
   if (argc > 2)
   {
     printf("Too many argument supplied.\n");
     exit(1);
   }
-  // if num of args is less than two,
+  // if num of args is less than two, ie 1 arg
   else if (argc < 2)
   {
     //assign dir name pointer to "."
@@ -40,6 +41,8 @@ int main(int argc, char **argv)
     printf("The argument supplied is %s\n", dirname);
   }
   // Open directory
+
+  // assign pointer for DIR type opendir function opens directory stream to dirname
   DIR *dir = opendir(dirname);
   // if opendir returns null, could not open directory
   if (dir == NULL)
@@ -47,16 +50,20 @@ int main(int argc, char **argv)
     printf("Could not open current directory");
     exit(1);
   }
+  //
 
   // Repeatly read and print entries
 
+  // loop until the de pointer or struct directory entry is pointing to the readdir function passed in  dir stream to dirname is not NULL
   while ((de = readdir(dir)) != NULL)
   {
-    printf("%s\n", de->d_name);
+    // printf("%s\n", de->d_name);
+    // syntax for function call pass in file name, and the stat struct
+    stat(de->d_name, &statbuf);
+    // print
+    printf("%ld\t%s\n", statbuf.st_size, de->d_name);
   }
-
   // Close directory
   closedir(dir);
-
   return 0;
 }
